7b3aee09785019faa0a786f2e4e5338e55d47eeb
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-04-20 18:48:29 +0800

    Update TimeNow-2.0.0.js

--- version/TimeNow-2.0.0.js
+++ version/TimeNow-2.0.0.js
@@ -1,39 +1,39 @@
 'use strict'
 
-const Plugin={
+const Plugin = {
     "name": "TimeNow",
     "version": "2.0.0",
-    "depends":{
-    "index":"1.X.X",
-    "pluginLoader":"2.X.X",
+    "depends": {
+        "index": "1.X.X",
+        "pluginLoader": "2.X.X",
     },
-    "Events":["messageCreate"],
-    "Commands":[
-    {
-        "name": "$time now",
-        "note": "查看現在時間"
-    }
+    "Events": ["messageCreate"],
+    "Commands": [
+        {
+            "name": "$time now",
+            "note": "查看現在時間"
+        }
     ],
     "author": ["whes1015"],
-    "link":"https://github.com/ExpTechTW/MPR-TimeNow",
-    "resources":["AGPL-3.0"],
-    "description":"顯示現在時間"
+    "link": "https://github.com/ExpTechTW/MPR-TimeNow",
+    "resources": ["AGPL-3.0"],
+    "description": "顯示現在時間"
 }
 
 
 const pluginLoader = require('../Core/pluginLoader')
 
 async function messageCreate(client, message) {
-    if(message.content=="$time now"){
-    let now = new Date()
-    let Now = now.getFullYear() +
-        "/" + (now.getMonth() + 1) +
-        "/" + now.getDate() +
-        " " + now.getHours() +
-        ":" + now.getMinutes() +
-        ":" + now.getSeconds()
-    message.reply(await pluginLoader.embed(Now, null, Info.author, "https://raw.githubusercontent.com/ExpTechTW/API/%E4%B8%BB%E8%A6%81%E7%9A%84-(main)/image/Icon/ExpTech.png"))
- }
+    if (message.content == "$time now") {
+        let now = new Date()
+        let Now = now.getFullYear() +
+            "/" + (now.getMonth() + 1) +
+            "/" + now.getDate() +
+            " " + now.getHours() +
+            ":" + now.getMinutes() +
+            ":" + now.getSeconds()
+        message.reply(await pluginLoader.embed(Now, null, Info.author, "https://raw.githubusercontent.com/ExpTechTW/API/%E4%B8%BB%E8%A6%81%E7%9A%84-(main)/image/Icon/ExpTech.png"))
+    }
 }
 
 module.exports = {
