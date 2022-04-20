bfa6d28109d51eb4cae0eab4f3a1fea568069b53
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-04-20 16:59:33 +0800

    update TimeNow-2.0.0.js

--- version/TimeNow-2.0.0.js
+++ version/TimeNow-2.0.0.js
@@ -1,81 +1,44 @@
 'use strict'
 
-
-
-const Info = {
-
-    "version": "1.0.1",
-
-    "pluginLoader":["1.X.X"],
-
+{
     "name": "TimeNow",
-
-    "author": "whes1015"
-
-}
-
-
-
-const Commands = [
-
+    "version": "2.0.0",
+    "depends":{
+    "index":"1.X.X",
+    "pluginLoader":"2.X.X",
+    },
+    "Events":["messageCreate"],
+    "Commands":[
     {
-
         "name": "$time now",
-
         "note": "查看現在時間"
-
     }
-
-]
-
-
-
-const Event = [
-
-    "messageCreate"
-
-]
-
+    ],
+    "author": [whes1015],
+    "link":""
+    "resources":["AGPL-3.0"]
+    "description":"顯示現在時間"
+}
 
 
 const pluginLoader = require('../Core/pluginLoader')
 
-
-
 async function messageCreate(client, message) {
-
     if(message.content=="$time now"){
-
     let now = new Date()
-
     let Now = now.getFullYear() +
-
         "/" + (now.getMonth() + 1) +
-
         "/" + now.getDate() +
-
         " " + now.getHours() +
-
         ":" + now.getMinutes() +
-
         ":" + now.getSeconds()
-
     message.reply(await pluginLoader.embed(Now, null, Info.author, "https://raw.githubusercontent.com/ExpTechTW/API/%E4%B8%BB%E8%A6%81%E7%9A%84-(main)/image/Icon/ExpTech.png"))
-
  }
-
 }
 
-
-
 module.exports = {
-
     Info,
-
     Commands,
-
     Event,
-
     messageCreate
-
 }
