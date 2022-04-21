b72e176b21d3b8a923eb0e4f39485c898457226f
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-04-21 17:39:56 +0800

    Create TimeNow-3.1.0.js

---
+++ version/TimeNow-3.1.0.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const Plugin = {
+    "name": "TimeNow",
+    "version": "3.1.0",
+    "depends": {
+        "pluginLoader": ">=3.5.0"
+    },
+    "Events": ["messageCreate"],
+    "Commands": [
+        {
+            "name": "time now",
+            "note": "查看現在時間"
+        }
+    ],
+    "author": ["whes1015"],
+    "link": "https://github.com/ExpTechTW/MPR-TimeNow",
+    "resources": ["AGPL-3.0"],
+    "description": "顯示現在時間"
+}
+
+
+const pluginLoader = require('../Core/pluginLoader')
+
+async function messageCreate(client, message) {
+    if (message.content == "time now") {
+        let utc = new Date()
+        let now = new Date(utc.getTime() + utc.getTimezoneOffset() * 60 * 1000 + 60 * 60 * 8 * 1000)
+        let Now = now.getFullYear() +
+            "/" + (now.getMonth() + 1) +
+            "/" + now.getDate() +
+            " " + now.getHours() +
+            ":" + now.getMinutes() +
+            ":" + now.getSeconds()
+        message.reply(await pluginLoader.embed(Now, null, Plugin.author.toString().replaceAll(",", " "), "https://raw.githubusercontent.com/ExpTechTW/API/%E4%B8%BB%E8%A6%81%E7%9A%84-(main)/image/Icon/ExpTech.png"))
+    }
+}
+
+module.exports = {
+    Plugin,
+    messageCreate
+}
